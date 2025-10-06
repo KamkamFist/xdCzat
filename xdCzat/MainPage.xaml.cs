131229a2aa73c91d9ad9d1ee29eadfcceca0c3e2
4313657 <4313657@TE-GDA-D-D21E01>
2025-10-06 15:56:38 +0200

    sfsdfsd

--- xdCzat/MainPage.xaml.cs
+++ xdCzat/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace xdCzat
+﻿
+
+namespace xdCzat
 {
     public partial class MainPage : ContentPage
     {
@@ -9,6 +11,16 @@
             InitializeComponent();
         }
 
-     
+        private void Send(object sender, EventArgs e)
+        {
+            string userName = UsernameEntry.Text;
+            string message = ChatEntry.Text;
+
+            ChatEntry.Text = string.Empty;
+
+            Label messageLabel = new Label();
+            messageLabel.Text = userName + ": " + message;
+            ChatHistory.Children.Add(messageLabel);
+        }
     }
 }
